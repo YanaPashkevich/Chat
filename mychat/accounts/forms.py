e05894ecf1f6c381fedda5d91b45cd2e5f9b34d8
YanaPashkevich <yana.pashkevich.96@gmail.com>
2022-10-12 20:09:40 +0300

    fixing templates again

--- mychat/accounts/forms.py
+++ mychat/accounts/forms.py
@@ -1,5 +1,3 @@
-from datetime import date, datetime
-from tkinter import Widget
 from django.contrib.auth.models import User
 from django import forms
 from .models import Profile
--- mychat/core/urls.py
+++ mychat/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('main/', views.index, name='index'),
     path('sign_up/', views.sign_up, name='sign_up')
-]
+
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
